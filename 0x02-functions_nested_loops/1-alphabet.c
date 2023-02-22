c6f72a32e77727478884a86d129cdeb3f31e5144
Cliffmisati <cliffemisati@gmail.com>
2023-02-21 16:42:27 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <main.h>
+/**
+ * print_alphabet - function that prints alphabet in lower case
+ *
+ * Return: always 0;
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a' ; i <= 'z' ; la++)
+		_putchar('la');
+
+	_putchar('\n');
+}
+
+
+
+
