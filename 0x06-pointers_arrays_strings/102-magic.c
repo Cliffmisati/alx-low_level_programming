76c00b178530126cf90ecb5cc726a099cc0a79fa
Cliffmisati <cliffemisati@gmail.com>
2023-03-03 17:49:23 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/**
+/*
  * This program demonstrates the use of a magic number.
  */
 
@@ -19,7 +19,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-	*(p + 5) = 98;
+	*(a + 2 + 3) = 98;
   /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
